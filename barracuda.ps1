1734d1122d57ccd3585dbbea334dfe0c991b6e7b
Luigi Rosa <luigirosa@users.noreply.github.com>
2025-09-12 12:06:58 +0200

    dbg

--- barracuda.ps1
+++ barracuda.ps1
@@ -67,7 +67,8 @@ if ($DBGdomain) {
             $res = $resRaw | ConvertFrom-Json
             $thispage = $res.pageNum
             $pagetot = $res.pagesTotal
-            $res = $ResRaw.Results | ConvertFrom-Json
+            $res | fl
+            $res = $ResRaw.results | ConvertFrom-Json
             foreach ($rec in $res) {
                 $qry = "INSERT INTO $SQLtablenetscan
                 ([timestamp],[domainId],[domainName],[status],[type])
